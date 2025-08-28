112cf6b5d1e2e34e94b06f1b4a80e66956bedeb7
azryl <azryl.career@gmail.com>
2025-08-28 21:52:17 +0800

    Create Gameboard and Cell object factory

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-console.log("File linking test successful");
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+
+    const getBoard = () => board;
+
+    const placeMark = (row, column, player) => {
+        const isEmptyCell = row[column].getValue() === ' ';
+
+        if (!isEmptyCell) return;
+
+        board[row][column].addMark(player);
+    };
+
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+        console.log(boardWithCellValues);
+    };
+
+
+    return { getBoard, placeMark, printBoard };
+}
+
+function Cell() {
+    let value = ' ';
+
+    const addMark = (player) => {
+        value = player;
+    };
+
+    const getValue = () => value;
+
+    return {
+        addMark,
+        getValue
+    };
+}
